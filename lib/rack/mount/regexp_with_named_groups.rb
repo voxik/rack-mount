20758ec4468507f8bc85ae0ad83028d02537dad8
Joshua Peek <josh@joshpeek.com>
2009-03-21 21:15:12 -0500

    moved named captured parsers into utils

--- lib/rack/mount/regexp_with_named_groups.rb
+++ lib/rack/mount/regexp_with_named_groups.rb
@@ -1,59 +1,6 @@
-require 'strscan'
-
 module Rack
   module Mount
     class RegexpWithNamedGroups < Regexp
-      # Old parser that extracted named captures from comments
-      def self.extract_comment_capture_names(regexp)
-        names, scanner, last_close = [], StringScanner.new(regexp.source), nil
-
-        while scanner.skip_until(/\(/)
-          next if scanner.pre_match =~ /\\$/
-
-          if scanner.scan(/\?\#(.+?)(?=\))/)
-            if scanner[1] =~ /^:(\w+)$/
-              names[last_close] = $1.to_s
-            end
-          else
-            names << :capture
-          end
-
-          while scanner.skip_until(/[()]/)
-            if scanner.matched =~ /\)$/
-              (names.size - 1).downto(0) do |i|
-                if names[i] == :capture
-                  names[last_close = i] = nil
-                  break
-                end
-              end
-            else
-              scanner.unscan
-              break
-            end
-          end
-        end
-
-        regexp = regexp.source.gsub(/\(\?#:[a-z]+\)/, '')
-        return Regexp.compile(regexp), names
-      end
-
-      def self.extract_named_captures(regexp)
-        names, scanner = [], StringScanner.new(regexp.source)
-
-        while scanner.skip_until(/\(/)
-          next if scanner.pre_match =~ /\\$/
-
-          if scanner.scan(/\?:<([^>]+)>/)
-            names << scanner[1]
-          else
-            names << nil
-          end
-        end
-
-        regexp = regexp.source.gsub(/\?:<[^>]+>/, '')
-        return Regexp.compile(regexp), names
-      end
-
       def initialize(regexp, names = nil)
         names = nil if names && !names.any?
 
@@ -65,7 +12,7 @@ module Rack
           @names = names.map { |n| n && n.to_s }
         else
           regexp = Regexp.compile(regexp)
-          regexp, @names = self.class.extract_named_captures(regexp)
+          regexp, @names = Utils.extract_named_captures(regexp)
         end
 
         @names = nil unless @names.any?
--- lib/rack/mount/utils.rb
+++ lib/rack/mount/utils.rb
@@ -1,3 +1,5 @@
+require 'strscan'
+
 module Rack
   module Mount
     module Utils
@@ -44,6 +46,59 @@ module Rack
         RegexpWithNamedGroups.new("^#{re}$")
       end
       module_function :convert_segment_string_to_regexp
+
+      # Old parser that extracted named captures from comments
+      def extract_comment_capture_names(regexp)
+        names, scanner, last_close = [], StringScanner.new(regexp.source), nil
+
+        while scanner.skip_until(/\(/)
+          next if scanner.pre_match =~ /\\$/
+
+          if scanner.scan(/\?\#(.+?)(?=\))/)
+            if scanner[1] =~ /^:(\w+)$/
+              names[last_close] = $1.to_s
+            end
+          else
+            names << :capture
+          end
+
+          while scanner.skip_until(/[()]/)
+            if scanner.matched =~ /\)$/
+              (names.size - 1).downto(0) do |i|
+                if names[i] == :capture
+                  names[last_close = i] = nil
+                  break
+                end
+              end
+            else
+              scanner.unscan
+              break
+            end
+          end
+        end
+
+        regexp = regexp.source.gsub(/\(\?#:[a-z]+\)/, '')
+        return Regexp.compile(regexp), names
+      end
+      module_function :extract_comment_capture_names
+
+      def extract_named_captures(regexp)
+        names, scanner = [], StringScanner.new(regexp.source)
+
+        while scanner.skip_until(/\(/)
+          next if scanner.pre_match =~ /\\$/
+
+          if scanner.scan(/\?:<([^>]+)>/)
+            names << scanner[1]
+          else
+            names << nil
+          end
+        end
+
+        regexp = regexp.source.gsub(/\?:<[^>]+>/, '')
+        return Regexp.compile(regexp), names
+      end
+      module_function :extract_named_captures
 
       def extract_static_segments(regexp)
         if regexp.to_s =~ %r{\(\?-mix:?(.+)?\)}
