ef836ed53ddce7000e7c42d8e784d1dcc7df8097
Joshua Peek <josh@joshpeek.com>
2009-04-05 11:12:23 -0500

    kill old regexp comment extractor

--- lib/rack/mount/utils.rb
+++ lib/rack/mount/utils.rb
@@ -47,41 +47,6 @@ module Rack
       end
       module_function :convert_segment_string_to_regexp
 
-      # Old parser that extracted named captures from comments
-      def extract_comment_capture_names(regexp)
-        names, scanner, last_close = [], StringScanner.new(regexp.source), nil
-
-        while scanner.skip_until(/\(/)
-          next if scanner.pre_match =~ /\\$/
-
-          if scanner.scan(/\?\#(.+?)(?=\))/)
-            if scanner[1] =~ /^:(\w+)$/
-              names[last_close] = $1.to_s
-            end
-          else
-            names << :capture
-          end
-
-          while scanner.skip_until(/[()]/)
-            if scanner.matched =~ /\)$/
-              (names.size - 1).downto(0) do |i|
-                if names[i] == :capture
-                  names[last_close = i] = nil
-                  break
-                end
-              end
-            else
-              scanner.unscan
-              break
-            end
-          end
-        end
-
-        regexp = regexp.source.gsub(/\(\?#:[a-z]+\)/, '')
-        return Regexp.compile(regexp), names
-      end
-      module_function :extract_comment_capture_names
-
       def extract_named_captures(regexp)
         names, scanner = [], StringScanner.new(regexp.source)
 
