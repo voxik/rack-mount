a8b72145e7915907dedc2bc3d375267bb875e3c9
Joshua Peek <josh@joshpeek.com>
2009-04-18 18:41:48 -0500

    move list unrolling into optimize_call! method

--- lib/rack/mount/route_set/optimizations.rb
+++ lib/rack/mount/route_set/optimizations.rb
@@ -3,40 +3,6 @@ module Rack
     class RouteSet
       module Optimizations
         def freeze
-          @recognition_graph.lists.each do |list|
-            body = (0...list.length).zip(list).map { |i, route|
-              assign_index_params = route.instance_variable_get("@named_captures").map { |k, index|
-                "routing_args[#{k.inspect}] = param_matches[#{index}] if param_matches[#{index}]"
-              }
-              <<-EOS
-                if #{route.method ? "method == #{route.method.inspect} && " : ''}path =~ #{route.path.inspect}
-                  route = self[#{i}]
-                  #{if assign_index_params.any?
-                    'routing_args, param_matches = route.defaults.dup, $~.captures'
-                  else
-                    'routing_args = route.defaults.dup'
-                  end}
-                  #{assign_index_params.join("\n                  ")}
-                  env[Const::RACK_ROUTING_ARGS] = routing_args
-                  result = route.app.call(env)
-                  return result unless result[0] == #{@catch}
-                end
-              EOS
-            }.join
-
-            method = <<-EOS, __FILE__, __LINE__
-              def optimized_each(env)
-                method = env[Const::REQUEST_METHOD]
-                path = env[Const::PATH_INFO]
-#{body}
-                nil
-              end
-            EOS
-
-            puts method if ENV[Const::RACK_MOUNT_DEBUG]
-            list.instance_eval(*method)
-          end
-
           optimize_call! unless frozen?
           super
         end
@@ -54,6 +20,40 @@ module Rack
 
         private
           def optimize_call!
+            @recognition_graph.lists.each do |list|
+              body = (0...list.length).zip(list).map { |i, route|
+                assign_index_params = route.instance_variable_get("@named_captures").map { |k, index|
+                  "routing_args[#{k.inspect}] = param_matches[#{index}] if param_matches[#{index}]"
+                }
+                <<-EOS
+                  if #{route.method ? "method == #{route.method.inspect} && " : ''}path =~ #{route.path.inspect}
+                    route = self[#{i}]
+                    #{if assign_index_params.any?
+                      'routing_args, param_matches = route.defaults.dup, $~.captures'
+                    else
+                      'routing_args = route.defaults.dup'
+                    end}
+                    #{assign_index_params.join("\n                  ")}
+                    env[Const::RACK_ROUTING_ARGS] = routing_args
+                    result = route.app.call(env)
+                    return result unless result[0] == #{@catch}
+                  end
+                EOS
+              }.join
+
+              method = <<-EOS, __FILE__, __LINE__
+                def optimized_each(env)
+                  method = env[Const::REQUEST_METHOD]
+                  path = env[Const::PATH_INFO]
+#{body}
+                  nil
+                end
+              EOS
+
+              puts method if ENV[Const::RACK_MOUNT_DEBUG]
+              list.instance_eval(*method)
+            end
+
             instance_eval(<<-EOS, __FILE__, __LINE__)
               def call(env)
                 req = Request.new(env)
