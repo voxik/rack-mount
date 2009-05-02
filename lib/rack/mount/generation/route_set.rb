110beed1dfcba78393e23579516a383408c4b89e
Joshua Peek <josh@joshpeek.com>
2009-05-01 19:17:16 -0500

    doc RouteSet generation methods

--- lib/rack/mount/generation/route_set.rb
+++ lib/rack/mount/generation/route_set.rb
@@ -2,17 +2,31 @@ module Rack
   module Mount
     module Generation
       module RouteSet
+        # Adds generation related concerns to RouteSet.new.
         def initialize(*args)
           @named_routes = {}
           super
         end
 
+        # Adds generation aspects to RouteSet#add_route.
         def add_route(*args)
           route = super
           @named_routes[route.name] = route if route.name
           route
         end
 
+        # Generates path from identifiers or significant keys.
+        #
+        # To generate a url by named route, pass the name in as a +Symbol+.
+        #   url_for(:dashboard) # => "/dashboard"
+        #
+        # Additional parameters can be passed in as a hash
+        #   url_for(:people, :id => "1") # => "/people/1"
+        #
+        # If no name route is given, it will fall back to a slower
+        # generation search.
+        #   url_for(:controller => "people", :action => "show", :id => "1")
+        #     # => "/people/1"
         def url_for(*args)
           params = args.last.is_a?(Hash) ? args.pop : {}
           named_route = args.shift
