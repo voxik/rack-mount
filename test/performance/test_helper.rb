3b6e759827fc3db701bcdcaf0284b2ba477495c6
Joshua Peek <josh@joshpeek.com>
2009-05-17 23:31:04 -0500

    ensure a uniq env is generated for each test

--- test/performance/test_helper.rb
+++ test/performance/test_helper.rb
@@ -8,9 +8,8 @@ require 'fixtures'
 
 module EnvGenerator
   def env_for(n, *args)
-    env = Rack::MockRequest.env_for(*args).freeze
     envs = []
-    n.times { envs << env.dup }
+    n.times { envs << Rack::MockRequest.env_for(*args) }
     envs
   end
   module_function :env_for
