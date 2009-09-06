ca17250590d9288e81958fe46567504d58b85251
Joshua Peek <josh@joshpeek.com>
2009-09-05 23:56:20 -0500

    make sure usher gets its own envs for the benchmark

--- test/performance/nested_recognition.rb
+++ test/performance/nested_recognition.rb
@@ -15,23 +15,29 @@ Ush = Usher::Interface.for(:rack) do
 end
 
 TIMES = 10_000.to_i
-FirstEnv = EnvGenerator.env_for(TIMES, '/a')
-MidEnv = EnvGenerator.env_for(TIMES, '/mn')
-LastEnv = EnvGenerator.env_for(TIMES, '/zz')
+
+MountFirstEnv = EnvGenerator.env_for(TIMES, '/a')
+UshFirstEnv = EnvGenerator.env_for(TIMES, '/a')
+
+MountMidEnv = EnvGenerator.env_for(TIMES, '/mn')
+UshMidEnv = EnvGenerator.env_for(TIMES, '/mn')
+
+MountLastEnv = EnvGenerator.env_for(TIMES, '/zz')
+UshLastEnv = EnvGenerator.env_for(TIMES, '/zz')
 
 Benchmark.bmbm do |x|
-  x.report('rack-mount (first)')  { TIMES.times { |n| Mount.call(FirstEnv[n]) } }
-  x.report('usher (first)') { TIMES.times { |n| Ush.call(FirstEnv[n]) } }
-  x.report('rack-mount (mid)')    { TIMES.times { |n| Mount.call(MidEnv[n]) } }
-  x.report('usher (mid)')   { TIMES.times { |n| Ush.call(MidEnv[n]) } }
-  x.report('rack-mount (last)')   { TIMES.times { |n| Mount.call(LastEnv[n]) } }
-  x.report('usher (last)')  { TIMES.times { |n| Ush.call(LastEnv[n]) } }
+  x.report('rack-mount (first)')  { TIMES.times { |n| Mount.call(MountFirstEnv[n]) } }
+  x.report('usher (first)') { TIMES.times { |n| Ush.call(UshFirstEnv[n]) } }
+  x.report('rack-mount (mid)')    { TIMES.times { |n| Mount.call(MountMidEnv[n]) } }
+  x.report('usher (mid)')   { TIMES.times { |n| Ush.call(UshMidEnv[n]) } }
+  x.report('rack-mount (last)')   { TIMES.times { |n| Mount.call(MountLastEnv[n]) } }
+  x.report('usher (last)')  { TIMES.times { |n| Ush.call(UshLastEnv[n]) } }
 end
 
 #                          user     system      total        real
-# rack-mount (first)   0.270000   0.000000   0.270000 (  0.279772)
-# usher (first)        1.800000   0.010000   1.810000 (  1.812982)
-# rack-mount (mid)     0.280000   0.000000   0.280000 (  0.280072)
-# usher (mid)          1.780000   0.000000   1.780000 (  1.784955)
-# rack-mount (last)    0.300000   0.000000   0.300000 (  0.308182)
-# usher (last)         1.730000   0.000000   1.730000 (  1.738455)
+# rack-mount (first)   0.310000   0.000000   0.310000 (  0.315686)
+# usher (first)        1.760000   0.010000   1.770000 (  1.761123)
+# rack-mount (mid)     0.310000   0.000000   0.310000 (  0.317276)
+# usher (mid)          1.880000   0.010000   1.890000 (  1.891970)
+# rack-mount (last)    0.320000   0.000000   0.320000 (  0.319082)
+# usher (last)         1.860000   0.000000   1.860000 (  1.871472)
