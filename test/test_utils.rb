6cfd09e9ab556ac95c0a38fc8ca57805d35ccb5c
Joshua Peek <josh@joshpeek.com>
2009-09-20 22:27:59 -0500

    some build query tests

--- test/test_utils.rb
+++ test/test_utils.rb
@@ -16,6 +16,18 @@ class TestUtils < Test::Unit::TestCase
     assert_equal [], pop_trailing_nils!([nil])
   end
 
+  def test_build_nested_query
+    assert_equal 'foo', build_nested_query('foo' => nil)
+    assert_equal 'foo=', build_nested_query('foo' => '')
+    assert_equal 'foo=bar', build_nested_query('foo' => 'bar')
+    assert_equal 'foo=1&bar=2', build_nested_query('foo' => '1', 'bar' => '2')
+    assert_equal 'my+weird+field=q1%212%22%27w%245%267%2Fz8%29%3F',
+      build_nested_query('my weird field' => 'q1!2"\'w$5&7/z8)?')
+    assert_equal 'foo%5B%5D', build_nested_query('foo' => [nil])
+    assert_equal 'foo%5B%5D=', build_nested_query('foo' => [''])
+    assert_equal 'foo%5B%5D=bar', build_nested_query('foo' => ['bar'])
+  end
+
   def test_regexp_anchored
     assert_equal true, regexp_anchored?(/^foo$/)
     assert_equal true, regexp_anchored?(/\Afoo\Z/)
