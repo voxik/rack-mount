a1fc64ee0e969752779062f2e62f64d96bf638be
Joshua Peek <josh@joshpeek.com>
2009-11-22 00:38:58 -0600

    fix broken benchmark helpers

--- test/fixtures/default_set.rb
+++ test/fixtures/default_set.rb
@@ -1,5 +1,5 @@
 DefaultSet = Rack::Mount::RouteSet.new do |set|
-  if supports_named_captures?
+  if Rack::Mount::RegexpWithNamedGroups.supports_named_captures?
     re = eval('%r{^/(?<controller>[a-z0-9]+)(/(?<action>[a-z0-9]+)(/(?<id>[a-z0-9]+)(\.(?<format>[a-z]+))?)?)?$}')
     set.add_route(EchoApp, :path_info => re)
   else
